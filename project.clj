aedf7bb66e71acaab540d21f7f29c0364801db06
Bart Kleijngeld <bartkl@gmail.com>
2022-04-22 15:49:29 +0200

    Removed redundant non-descriptive core file and entry point

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :repl-options {:init-ns schema-transformer.core}
-  :main schema-transformer.core/foo)
+  ;; :main schema-transformer.core/foo
+  )
--- src/schema_transformer/core.clj
+++
@@ -1,6 +0,0 @@
-(ns schema-transformer.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
