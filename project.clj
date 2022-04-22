fe96a30950b352a7e74dbba97fd667c67fd7f9ca
Bart Kleijngeld <bartkl@gmail.com>
2022-04-22 16:08:10 +0200

    Clojure upgrade and added rdf4j deps

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns schema-transformer.core}
-  ;; :main schema-transformer.core/foo
-  )
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.eclipse.rdf4j/rdf4j-rio-turtle "4.0.0-M3"]]
+  :repl-options {:init-ns schema-transformer.core})
