ade99ef146325b6cdf67a2f76297d04aee7d9774
Bart Kleijngeld <bartkl@gmail.com>
2022-05-19 17:12:57 +0200

    Loads of stuff, but cardinalities and entity recognition works

--- src/schema_transformer/asami.clj
+++ src/schema_transformer/asami.clj
@@ -19,12 +19,6 @@
 
 (def conn (d/connect db-uri))
 
-;; (def model
-;;   (let [m (rdf/read-directory (io/file "resources/example-profile/")
-;;                               (fn [path] (into-array IRI [(utils/iri-from-filename path)])))]
-;;     (map #(assoc {} :db/ident (:rdf/subject %) (:rdf/predicate %) (:rdf/object %)) m)))
-
-
 (def model
   (rdf/read-directory (io/file "resources/example-profile/")))
 
@@ -33,97 +27,133 @@
 @(d/transact conn {:tx-triples model})
 
 
-
-@(d/transact conn {:tx-triples [
-  [:https://w3id.org/schematransform/ExampleShape#BShape :id :https://w3id.org/schematransform/ExampleShape#BShape]
-  [:https://w3id.org/schematransform/ExampleShape#DShape :id :https://w3id.org/schematransform/ExampleShape#DShape]
-  [:https://w3id.org/schematransform/ExampleShape#BShape :a/entity true]
-  [:https://w3id.org/schematransform/ExampleShape#DShape :a/entity true]]})
-
 (defn bnode? [kw]
      (clojure.string/starts-with? (str kw) ":_:"))
 
 (defn get-resources [conn]
-    (map first 
-        (d/q '[:find ?e
-            :in $ ?is-bnode
-            :where [?e _ _]
-                    (not [(?is-bnode ?e)])]
-            conn bnode?)))
+    (map first
+         (d/q '[:find ?e
+                ;; :in $ ?is-bnode
+                :where [?e _ _]]
+              conn)))
+                ;; (not [(?is-bnode ?e)])]
+              ;; conn bnode?)))
 
 (defn mark-resource-as-entity [r] (vector r :a/entity true))
 (defn add-id [r] (vector r :id r))
 
-(mapcat #(list
-          (mark-resource-as-entity %)
-          (add-id %))
-        (get-resources conn))
+(defn mark-resources-as-entities [conn]
+  @(d/transact conn {:tx-triples
+                     (mapcat #(list
+                               (mark-resource-as-entity %)
+                               (add-id %))
+                             (get-resources conn))}))
 
+(mark-resources-as-entities conn)
 
 (d/q '[:find ?s ?p ?o :where [?s ?p ?o]] conn)
-;; (d/q '[:find ?t :where [:https://w3id.org/schematransform/ExampleShape#AShape :sh/targetClass ?t]] conn)
+;;;;;;;;;;;;;;;;; AVRO.
 
-;; (.toString RDF/TYPE)  ;; Does not work because `q` is a macro I guess?
-
-;; (as->gg
-;;  (d/q '[:find [?r ...] ;;?label ?comment
-;;         :where
-;;         [?r "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" "http://www.w3.org/ns/shacl#NodeShape"]
-;;         [?r "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" "http://www.w3.org/ns/shacl#NodeShape"]
-
-;;         ;; [?r "http://www.w3.org/2000/01/rdf-schema#comment" "\"RootObject\"^^<http://www.w3.org/2001/XMLSchema#string>"]
-;;         [?r "http://www.w3.org/ns/shacl#targetClass" ?targetClass]
-;;         [?r "http://www.w3.org/ns/shacl#property" * ?p]
-;;         ;; [?targetClass "http://www.w3.org/2000/01/rdf-schema#comment" ?commen
-;;         ;; [?targetClass "http://www.w3.org/2000/01/rdf-schema#label" ?label]
-;;         ]conn) v
-;;   (map #(d/entity conn % true) v) (first v))
-      ;; (first v)
-;;  (d/entity conn v true))
-
-(d/entity (d/db conn) :https://w3id.org/schematransform/ExampleShape#BShape true)
-;; (def a-shape (d/entity (d/db conn) :https://w3id.org/schematransform/ExampleShape#AShape false))
-(def start-node (d/entity (d/db conn) :https://w3id.org/schematransform/ExampleShape#EShape false))
-(d/entity (d/db conn) (java.net.URI. "https://w3id.org/schematransform/ExampleShape#idShape") true)
-
-
-(c-shape :id)
-
-;; (->>
-;; (map #(d/entity conn %) (d/q '[:find [?x ...] :where [?x ?y "https://w3id.org/schematransform/ExampleVocabulary#D"]] conn))
-;; (take 1)
-;;  )
-
-(defn target-class-iri [node conn]
-  (d/q '[:find ?t .
-         :in $ ?node
-         :where [?node :sh/targetClass ?t]]
-       conn node))
-
-(defn record-name [iri-kw]
-  (as-> (str iri-kw) v
+(defn iri-local-name [kw]
+  (as-> (str kw) v
     (subs v 1)
     (URI. v)
     (.getFragment v)
     (keyword v)))
 
-(defn record-doc [node] (get-in node [:sh/targetClass :rdfs/comment]))
-(defn avro-field [node]
-    (l/string-schema "Hallo"))
+(defn record-name [node]
+  (iri-local-name
+   (get-in node [:sh/targetClass :id :id])))
 
-(defn avro-schema [node name conn]
+(defn record-doc [node]
+  (let [target-class (get node :sh/targetClass)]
+    (target-class :rdfs/comment)))
+
+(def datatype-sh->avro
+  #:xsd{:string l/string-schema
+        :double l/double-schema
+        :boolean l/boolean-schema
+        :decimal l/bytes-schema
+        :float l/float-schema
+        :duration l/fixed-schema
+        :dateTime l/string-schema
+        :date l/string-schema
+        :time l/string-schema
+        :anyURI l/string-schema})
+
+(defn- field-name [node]
+  (iri-local-name (get-in node [:sh/path :id :id])))
+
+(def node {:sh/datatype :xsd/float :sh/minCount 1 :sh/maxCount 2})
+
+
+(def cardinality->schema-fn
+  {[1  1] identity
+   [0  1] l/maybe
+   [1 :*] l/array-schema
+   [0 :*] (comp l/maybe l/array-schema)})
+
+(defn- field-schema [node]
+(let [min-count (node :sh/minCount)
+      max-count (node :sh/maxCount)
+      schema (condp #(get %2 %1) node
+               :sh/datatype :>> datatype-sh->avro
+               :sh/node :>> str
+               nil)]
+  ((cardinality->schema-fn [(min min-count 1) (if (> max-count 1) :* max-count)]) schema)))
+
+(field-schema node)
+(#(get %2 %1) :sh/node node)
+
+
+
+(defn avro-field [node]
+    [(field-name node)
+     (field-schema node)])
+
+(def node (first test-props))
+
+(defn rdf-list->seq [rdf-list]
+  (loop [l rdf-list
+         s (list)]
+    (if (= l :rdf/nil)
+      s
+      (recur (l :rdf/rest) (conj s (l :rdf/first))))))
+
+(defn get-inherited-props [shape]
+  ())
+  ;; (loop [other-shapes shape
+  ;;        props (list)]
+  ;;   (rdf-list->seq (shape :sh/and))))
+
+
+(defn get-properties [node]
+  (concat
+   (:sh/property node)
+   (get-inherited-props node)))
+
+(defn avro-schema [root-node]
   (l/record-schema
-   (record-name (target-class-iri name conn))
-   (record-doc node)
-   (map avro-field (:sh/property node))))
+   (record-name root-node)
+   (record-doc root-node)
+   (map avro-field (get-properties root-node))))
+   
 
 ;; (l/default-data B)
 ;; (l/edn B)
 
-(def start-node (d/entity (d/db conn) :https://w3id.org/schematransform/ExampleShape#EShape false))
+(def start-node (d/entity conn :https://w3id.org/schematransform/ExampleShape#BShape true))
+(def x (d/entity conn :https://w3id.org/schematransform/ExampleShape#AShape true))
+(def root-node start-node)
 (def node start-node)
 
-(def a (avro-schema start-node :https://w3id.org/schematransform/ExampleShape#AShape conn))
+(def a (avro-schema start-node))
 (l/edn a)
 
 ;; (record-name (target-class-iri :https://w3id.org/schematransform/ExampleShape#CShape conn))
+(def node {:sh/maxCount "\"1\"^^<http://www.w3.org/2001/XMLSchema#integer>",
+  :sh/datatype :xsd/string,
+  :rdf/type :sh/PropertyShape,
+  :sh/minCount "\"1\"^^<http://www.w3.org/2001/XMLSchema#integer>",
+  :sh/path :https://w3id.org/schematransform/ExampleVocabulary#id,
+  :id {:id :https://w3id.org/schematransform/ExampleShape#idShape}})
