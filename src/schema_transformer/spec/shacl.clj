09ff5af8c1a2c961341ac01b093410301f7ec220
Bart Kleijngeld <bartkl@gmail.com>
2022-06-05 14:56:22 +0200

    Got only-one-key-of to work and decent iri and xsd

--- src/schema_transformer/spec/shacl.clj
+++ src/schema_transformer/spec/shacl.clj
@@ -1,26 +1,49 @@
 (ns schema-transformer.spec.shacl
   (:require [clojure.spec.alpha :as s]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.set :as set]
+            [schema-transformer.spec.xsd])
+  (:import (java.net URI URISyntaxException)))
 
-(s/def :sh/targetClass ::iri)
-(s/def :sh/and (s/* ::iri))
+;; (defn specs-for-ns [ns']
+;;   (into {}
+;;         (filter #(= (namespace (first %)) ns')
+;;                 (s/registry))))
+
+(defn iri? [kw]
+  (and (keyword? kw)
+       (some? (try (uri? (URI. (name kw)))
+                   (catch URISyntaxException e)))))
+
+;; (defn only-one [kws]
+;;   (fn [m]
+;;     (not= (map #(contains? m %) kws)
+;;           [true true])))
+
+(defn only-one-key-of [kws]
+  (fn [m]
+    (not
+     (set/subset? kws
+                  (into #{} (keys m))))))
+
+(s/def :sh/targetClass iri?)
+(s/def :sh/and (s/* iri?))
 (s/def :rdfs/comment string?)
 (s/def :rdfs/label string?)
 (s/def :sh/property (s/* :sh/propertyShape))
-
-(s/def :sh/path ::iri)
-(s/def :sh/datatype #{:xsd/string :xsd/double :xsd/boolean
-                      :xsd/int :xsd/decimal :xsd/float :xsd/duration
-                      :xsd/dateTime :xsd/date :xsd/time :xsd/anyURI})
-(s/def :sh/node ::iri)
+(s/def :sh/in (s/cat))
+(s/def :sh/path iri?)
+(s/def :sh/datatype :xsd/datatype)
+(s/def :sh/node iri?)
 (s/def :sh/minCount int?)
 (s/def :sh/maxCount int?)
-(s/def :sh/in (s/cat))
 
 (s/def :sh/propertyShape
-  (s/keys :req [:sh/path]
-          :opt [:sh/node :sh/minCount :sh/maxCount]))
-          ;; TODO: How can I say: one of (and not both) `:sh/node` or `sh:datatype` is required?
+  (s/and
+   (s/keys :req [:sh/path]
+           :opt [:sh/node :sh/datatype :sh/minCount :sh/maxCount])
+   (only-one-key-of [:sh/node :sh/datatype])))
+
 
 (s/def :sh/nodeShape
   (s/keys :req [:sh/targetClass]
