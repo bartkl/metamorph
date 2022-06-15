3abb2198de1f2defda82a1864a2ae4bcd6b2da1f
Bart Kleijngeld <bartkl@gmail.com>
2022-06-15 19:14:16 +0200

    Fk refs working

--- src/schema_transformer/schemas/sql/schema.clj
+++ src/schema_transformer/schemas/sql/schema.clj
@@ -2,34 +2,50 @@
   (:require [honey.sql :as sql]
             [honey.sql.helpers :as h]
             [schema-transformer.graph.shacl :as graph.shacl]
+            [schema-transformer.graph.db :as graph.db]
             [schema-transformer.utils.uri :as utils.uri]))
 
 (def xsd->sql
-  #:xsd{:string :varchar
+  #:xsd{:string [:varchar 255]
         :boolean :boolean
         :decimal :decimal
-        :float :float
-        :double :double})
+        :float [:float 63 6]
+        :int [:int 31]
+        :double [:double 63 6]
+        :duration [:double 63 6]
+        :dateTime :datetime
+        :date :datetime
+        :time :datetime
+        :anyURI [:varchar 255]})
 
-(defn id-column [] [:id :varchar 255 [:not nil] [:primary-key]])
+(defn id-column [] [:id [:varchar 255] [:not nil] [:primary-key]])
 
 (defn foreign-key-column [property-shape])
 
 (defn primitive-column [property-shape])
 
+(declare table)
+
+(defn class-name [class]
+  (utils.uri/iri-local-name (get-in class [:id :id])))
+
 (defn column [property-shape]
-  (let [name (utils.uri/iri-local-name
-              (get-in property-shape
-                      [:sh/path :id :id]))]
-    [name :varchar 255 [:not nil]]))
+  (let [name (class-name (:sh/path property-shape))
+        datatype (property-shape :sh/datatype)
+        node (property-shape :sh/node)
+        type (cond
+               (some? datatype) (xsd->sql datatype)
+               (not (graph.db/node-ref? node)) (class-name node))]
+
+    [name type]))
 
 (defn table [node-shape]
   (let [target-class (utils.uri/iri-local-name
                       (get-in node-shape
                               [:sh/targetClass :id :id]))]
 
-    (sql/format (h/create-table target-class
-                                (h/with-columns (cons
-                                                 (id-column)
-                                                 (map column (graph.shacl/properties node-shape)))))
-                :pretty true)))
+    (h/create-table
+     target-class
+     (h/with-columns (cons
+                      (id-column)
+                      (map column (graph.shacl/properties node-shape)))))))
