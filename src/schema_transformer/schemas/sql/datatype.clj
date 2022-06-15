5c1b158f8f1b5e0e924606d1d3928f57dfe212da
Bart Kleijngeld <bartkl@gmail.com>
2022-06-15 19:34:31 +0200

    Fixed bug: there is no tree here, so no need to prevent inf. recursion

---
+++ src/schema_transformer/schemas/sql/datatype.clj
@@ -0,0 +1,14 @@
+(ns schema-transformer.schemas.sql.datatype)
+
+(def xsd->sql
+  #:xsd{:string [:varchar 255]
+        :boolean :boolean
+        :decimal :decimal
+        :float [:float 63 6]
+        :int [:int 31]
+        :double [:double 63 6]
+        :duration [:double 63 6]
+        :dateTime :datetime
+        :date :datetime
+        :time :datetime
+        :anyURI [:varchar 255]})
--- src/schema_transformer/schemas/sql/schema.clj
+++ src/schema_transformer/schemas/sql/schema.clj
@@ -2,21 +2,9 @@
   (:require [honey.sql :as sql]
             [honey.sql.helpers :as h]
             [schema-transformer.graph.shacl :as graph.shacl]
+            [schema-transformer.schemas.sql.datatype :refer [xsd->sql]]
             [schema-transformer.graph.db :as graph.db]
             [schema-transformer.utils.uri :as utils.uri]))
-
-(def xsd->sql
-  #:xsd{:string [:varchar 255]
-        :boolean :boolean
-        :decimal :decimal
-        :float [:float 63 6]
-        :int [:int 31]
-        :double [:double 63 6]
-        :duration [:double 63 6]
-        :dateTime :datetime
-        :date :datetime
-        :time :datetime
-        :anyURI [:varchar 255]})
 
 (defn id-column [] [:id [:varchar 255] [:not nil] [:primary-key]])
 
@@ -26,16 +14,16 @@
 
 (declare table)
 
-(defn class-name [class]
+(defn- class-name [class]
   (utils.uri/iri-local-name (get-in class [:id :id])))
 
-(defn column [property-shape]
+(defn- column [property-shape]
   (let [name (class-name (:sh/path property-shape))
         datatype (property-shape :sh/datatype)
         node (property-shape :sh/node)
         type (cond
                (some? datatype) (xsd->sql datatype)
-               (not (graph.db/node-ref? node)) (class-name node))]
+               (some? node) (class-name node))]
 
     [name type]))
 
@@ -45,3 +33,4 @@
      (h/with-columns (cons
                       (id-column)
                       (map column (graph.shacl/properties node-shape))))))
+
