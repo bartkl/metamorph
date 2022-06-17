282750cb5310716864ebfe64ee9ac0faa94bf0af
Bart Kleijngeld <bartkl@gmail.com>
2022-06-17 20:58:41 +0200

    pkey/fkey working it seems

--- src/schema_transformer/schemas/sql/schema.clj
+++ src/schema_transformer/schemas/sql/schema.clj
@@ -2,35 +2,49 @@
   (:require [clojure.string :as string]
             [honey.sql :as sql]
             [honey.sql.helpers :as h]
-            [schema-transformer.graph.shacl :as graph.shacl]
+            [schema-transformer.graph.shacl :as shacl]
             [schema-transformer.schemas.sql.datatype :refer [xsd->sql]]))
 
-(defn id-column [] [:id [:varchar 255] [:not nil] [:primary-key]])
-(defn- key-ref [t] (keyword (str t "-" "id")))
+(defn- key-ref [t]
+  (keyword (str (name t) "-" "id")))
 
-(defn foreign-key-column [node-shape]
-  (let [name (graph.shacl/class-name node-shape)]
+(defn- foreign-key-column [node-shape]
+  (let [name (shacl/class-name node-shape)]
     [(key-ref name)]))
-
-(defn primitive-column [property-shape])
 
 (declare table)
 
-(defn- column [property-shape]
-  (let [name (graph.shacl/class-name (:sh/path property-shape))
+(defn- pkey? [property-shape]
+  (= (property-shape :rdfs/comment) "PrimaryKey"))
+
+(defn- fkey? [property-shape]
+  (contains? property-shape :sh/node))
+
+(defn pkey [node-shape]
+  (some->> (get node-shape :sh/property)
+           (filter pkey?)
+           first
+           :sh/path
+           shacl/class-name))
+
+(defn column [property-shape]
+  (let [name (shacl/class-name (:sh/path property-shape))
         type (condp #(%1 %2) property-shape
                :sh/datatype :>> xsd->sql
-               :sh/node :>> foreign-key-column
+               :sh/node :>> (fn [_] [:varchar 255])
                nil)]
+    (cond-> [name type]
+      (pkey? property-shape) (conj [:primary-key])
+      (fkey? property-shape) (conj [:foreign-key]
+                                   [:references
+                                    (shacl/class-name (get-in property-shape [:sh/node :sh/targetClass]))
+                                    (or (pkey (property-shape :sh/node)) :none)]))))
 
-    [name type]))
 
 (defn table [node-shape]
   (h/create-table
-   (graph.shacl/class-name (node-shape :sh/targetClass))
-   (h/with-columns (cons
-                    (id-column)
-                    (map column (graph.shacl/properties node-shape))))))
+   (shacl/class-name (node-shape :sh/targetClass))
+   (h/with-columns (map column (shacl/properties node-shape)))))
 
 ;; (defn schema [node-shapes]
 ;;   ;; (reduce (partial string/join ";") (->> node-shapes
