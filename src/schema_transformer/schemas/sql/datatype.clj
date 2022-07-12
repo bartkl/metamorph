a891ea2c50ca33c0f84c5760453125e5b44edc10
Bart Kleijngeld <bartkl@gmail.com>
2022-07-12 16:54:00 +0200

    Updated copyright headers

--- src/schema_transformer/schemas/sql/datatype.clj
+++ src/schema_transformer/schemas/sql/datatype.clj
@@ -1,3 +1,7 @@
+; SPDX-FileCopyrightText: 2022 Alliander N.V.
+;
+; SPDX-License-Identifier: Apache-2.0
+
 (ns schema-transformer.schemas.sql.datatype)
 
 (def xsd->sql
--- src/schema_transformer/schemas/sql/schema.clj
+++ src/schema_transformer/schemas/sql/schema.clj
@@ -1,3 +1,7 @@
+; SPDX-FileCopyrightText: 2022 Alliander N.V.
+;
+; SPDX-License-Identifier: Apache-2.0
+
 (ns schema-transformer.schemas.sql.schema
   (:require [clojure.string :as string]
             [honey.sql :as sql]
