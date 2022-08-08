30d9fd8935e690b990dd24b40aa6b8da566740b0
Bart Kleijngeld <bartkl@gmail.com>
2022-08-08 18:15:00 +0200

    Param refactor for SQL
    Signed-off-by: Bart Kleijngeld <bartkl@gmail.com>

--- src/metamorph/schemas/sql/schema.clj
+++ src/metamorph/schemas/sql/schema.clj
@@ -16,10 +16,10 @@
 (defn max-count [p]
   (if (= 1 (p :sh/maxCount)) 1 ##Inf))
 
-(defn node-name [n]
+(defn node-shape-name [n]
   (shacl/class-name (n :sh/targetClass)))
 
-(defn property-name [p]
+(defn property-shape-name [p]
   (shacl/class-name (p :sh/path)))
 
 (defn primary-key? [p]
@@ -46,15 +46,15 @@
        (rdf-list->seq (:sh/in n))))
 
 (defn node-shape->enum [n]
-  (let [create-ddl (h/create-table (node-name n)
+  (let [create-ddl (h/create-table (node-shape-name n)
                                    [:value [:varchar 255] [:primary-key]])
-        insert-ddl (-> (h/insert-into (node-name n))
+        insert-ddl (-> (h/insert-into (node-shape-name n))
                        (h/values [(into [] (enum-members n))]))]
     [create-ddl insert-ddl]))
 
 (defn property-shape->column [p]
   (letfn [(primitive-type [p] (xsd->sql (p :sh/datatype)))]
-    (cond-> [(property-name p)]
+    (cond-> [(property-shape-name p)]
       (contains? p :sh/datatype) (conj (primitive-type p))
       (primary-key? p) (conj [:primary-key])
       (contains? p :sh/node) (conj
@@ -62,42 +62,42 @@
                                 [:varchar 255]
                                 (primitive-type (foreign-key p)))
 
-                              [:foreign-key] [:references (node-name (p :sh/node))
+                              [:foreign-key] [:references (node-shape-name (p :sh/node))
                                               (if (foreign-key? p)
-                                                (property-name (foreign-key p))
+                                                (property-shape-name (foreign-key p))
                                                 :value)])
       (= 0 (min-count p)) (conj :null)
       (and (not (primary-key? p))
            (> (min-count p) 0)) (conj [:not nil]))))
 
 (defn node-shape->table [n]
-  (h/create-table (node-name n)
+  (h/create-table (node-shape-name n)
                   (h/with-columns (map property-shape->column
                                        (filter #(= 1 (max-count %))
                                                (shacl/properties n))))))
 
 (defn node-shape->link-table [left-node p]
-  (let [left-table (name (node-name left-node))
+  (let [left-table (name (node-shape-name left-node))
         left-datatype (xsd->sql ((primary-key left-node) :sh/datatype))
-        left-primary-key (name (property-name (primary-key left-node)))
+        left-primary-key (name (property-shape-name (primary-key left-node)))
         left-col (keyword (str left-table "_" left-primary-key))
         right (cond
-                (contains? p :sh/datatype) (name (property-name p))
-                (contains? p :sh/node) (name (node-name (p :sh/node))))
+                (contains? p :sh/datatype) (name (property-shape-name p))
+                (contains? p :sh/node) (name (node-shape-name (p :sh/node))))
         right-datatype (cond (contains? p :sh/datatype) (xsd->sql (p :sh/datatype))
                              (contains? p :sh/node) (xsd->sql ((foreign-key p) :sh/datatype)))
-        right-primary-key (when (contains? p :sh/node) (name (property-name (foreign-key p))))
+        right-primary-key (when (contains? p :sh/node) (name (property-shape-name (foreign-key p))))
         right-col (cond
                     (contains? p :sh/datatype) :value
                     (contains? p :sh/node) (keyword (str right "_" right-primary-key)))]
 
-    (-> (h/create-table (property-name p))
+    (-> (h/create-table (property-shape-name p))
         (h/with-columns
           [[left-col left-datatype [:foreign-key] [:references (keyword left-table) (keyword left-primary-key)]]
            (if (some? right-primary-key)
              [right-col right-datatype [:foreign-key] [:references (keyword right) (keyword right-primary-key)]]
              [right-col right-datatype])
-           [[:constraint (keyword (str (name (property-name p)) "_" "pkey"))] [:primary-key left-col right-col]]]))))
+           [[:constraint (keyword (str (name (property-shape-name p)) "_" "pkey"))] [:primary-key left-col right-col]]]))))
 
 (defn node-shape->link-tables [n]
   (reduce conj [] (map #(node-shape->link-table n %)
