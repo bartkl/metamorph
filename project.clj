ce60fb7d4e10d19f7377a9288fb04bcfbe444a36
Bart Kleijngeld <bartkl@gmail.com>
2022-08-10 09:54:15 +0200

    Added koacha and initial config

--- project.clj
+++ project.clj
@@ -9,10 +9,12 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-cljfmt "0.8.2"]]
   :main ^:skip-aot metamorph.core
+  :profiles {:dev {:dependencies [[lambdaisland/kaocha "1.69.1069"]]}}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.eclipse.rdf4j/rdf4j-rio-turtle "4.0.0"]
                  [deercreeklabs/lancaster "0.9.17"]
                  [ont-app/vocabulary "0.1.7"]
                  [cli-matic "0.5.3"]
                  [org.clojars.quoll/asami "2.3.2"]
-                 [com.github.seancorfield/honeysql "2.2.891"]])
+                 [com.github.seancorfield/honeysql "2.2.891"]]
+  :aliases {"kaocha" ["run" "-m" "kaocha.runner"]})
