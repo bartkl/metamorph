54eb47249e84bd1a81623b747d3ec94c8a80dfc5
Bart Kleijngeld <bartkl@gmail.com>
2022-09-19 14:54:25 +0200

    Trying to find bug

--- src/metamorph/core.clj
+++ src/metamorph/core.clj
@@ -3,23 +3,79 @@
 ; SPDX-License-Identifier: Apache-2.0
 
 (ns metamorph.core
-  (:require [cli-matic.core :refer [run-cmd]]
+  (:require [cli-matic.core :refer [run-cmd run-cmd*]]
+            [clojure.spec.alpha :as spec]
+            [expound.alpha :as expound]
+            [metamorph.utils.spec :refer [one-key-of]]
+            [metamorph.utils.cli :refer [kw->opt]]
+            [clojure.tools.cli :refer [parse-opts]]
             [honey.sql :as sql]
             [deercreeklabs.lancaster :as l]
+            [clojure.string :as str]
             [asami.core :as d]
             [clojure.java.io :as io]
             [ont-app.vocabulary.core :as vocab]
-            [metamorph.cli :as cli]
             [metamorph.rdf.reading :as rdf]
             [metamorph.schemas.avro.schema :refer [avro-schema]]
             [metamorph.schemas.sql.schema :as sql.schema]
             [metamorph.graph.shacl :as graph.shacl]
+            [metamorph.graph.avro :as graph.avro]
             [metamorph.graph.db :as graph.db]
             [metamorph.vocabs.prof :as prof]
-            [metamorph.vocabs.role :as role]
-            [metamorph.cli :as cli])
+            [metamorph.vocabs.role :as role])
   (:gen-class))
 
+(def input-sources #{:shacl :dx-profile})
+
+(spec/def ::command-args
+  (one-key-of (vec input-sources)))
+
+(expound/defmsg ::command-args
+  (str
+   "Please provide exactly one input.\n"
+   "Choices:\n\t"
+   (str/join "\n\t" (map kw->opt input-sources))))
+
+(defn generate-schema [schema]
+  (fn [{:keys [dx-profile shacl]}]
+   (let [db-uri "asami:mem://logical-model"
+         logical-model (rdf/read-triples (io/file (or dx-profile shacl)))]
+     (d/create-database db-uri)
+     (let [conn (d/connect db-uri)
+           root-uri (graph.avro/root-node-shape-uri conn)]
+       (graph.db/store-resources! conn logical-model)
+       (case schema
+         :avro ((->> (graph.db/resource conn root-uri)
+                 (avro-schema)
+                 (l/json)
+                 (spit "testBShape.json"))))))))
+
+  
+
+(def command-spec
+  {:command "metamorph"
+   :description "Generate a schema of a variety of formats, from a DX Profile or SHACL model."
+   :version "0.1.0"
+   :opts [{:as "Input: DX Profile directory"
+           :option "dx-profile"
+           :type :string}
+          {:as "Input: SHACL file"
+           :option "shacl"
+           :type :string}]
+   :subcommands [{:command "avro"
+                  :description "Apache Avro schema"
+                  :opts [{:as "Serialization format"
+                          :default :json
+                          :option "format"
+                          :short  "f"
+                          :type #{:edn :json}}
+                         {:as "Output file"
+                          :default "./avro.json"
+                          :option "output"
+                          :short  "o"
+                          :type :string}]
+                  :runs generate-schema}]
+   :spec ::command-args})
 
 (defn -main
   "This is our entry point.
@@ -27,48 +83,36 @@
   Commands (functions) will be invoked as appropriate."
   [& args]
 
-  (println args)  ;; NOTE: Remove when done with developing.
-  (run-cmd args cli/command-spec))
+  (run-cmd command-spec args))
 
-  ;; TODO: Implement logic that, dependent on the CLI args, performs the schema generation.
-  ;; Example for Avro:
-  ;;
-  ;; (let [db-uri "asami:mem://profile"
-  ;;       data (rdf/read-triples (io/file "resources/example_profile/"))]
-  ;;   (d/create-database db-uri)
-  ;;
-  ;;   (let [conn (d/connect db-uri)]
-  ;;     data
-  ;;     @(d/transact conn {:tx-triples data})
-  ;;     (graph.db/mark-resources-as-entities conn)
-  ;;
-  ;;     (->> (d/entity conn (vocab/keyword-for "https://w3id.org/schematransform/ExampleShape#BShape") true)
-  ;;          (avro-schema)
-  ;;          (l/edn)))))
-
+;; Playground.
 (comment
-  (def db-uri "asami:mem://profile")
+  (def db-uri "asami:mem://logical-model")
   (d/create-database db-uri)
   (d/delete-database db-uri)
 
   (def conn (d/connect db-uri))
 
   (def model
-    (rdf/read-triples (io/file "/home/bartkl/Programming/alliander-opensource/SchemaTransformer/app/src/test/resources/rdfs")))
+    (rdf/read-triples (io/file "/home/bartkl/Programming/alliander-opensource/metamorph/dev-resources/example_profile")))
+    ;; (rdf/read-triples (io/file "/home/bartkl/Programming/alliander-opensource/SchemaTransformer/app/src/test/resources/rdfs")))
 
   (take 20 model)
   (graph.db/store-resources! conn model)
+  (graph.avro/root-node-shape-uri conn)
 
-  (def b-shape (graph.db/resource conn (vocab/keyword-for "https://w3id.org/schematransform/ExampleShape#BShape")))
+  ;; (def b-shape (graph.db/resource conn (vocab/keyword-for "https://w3id.org/schematransform/ExampleShape#BShape")))
+  (def b-shape-uri (graph.avro/root-node-shape-uri conn))
+  (def b-shape (graph.db/resource conn b-shape-uri))
 
-  ;; Avro  .
+  ;; Avro.
   (def s (avro-schema b-shape))
   (l/edn s)
   (spit "testBShape.json" (l/json s))
 
   ;; SQL.
   (def node-shapes-names
-    (flatten (graph.shacl/get-node-shapes conn)))
+    (graph.shacl/get-node-shapes conn))
 
   (def node-shapes (->> node-shapes-names
                         (map #(d/entity conn % true))))
@@ -77,4 +121,17 @@
   (sql/format (sql.schema/node-shape->table b-shape))
 
   (->> (sql.schema/sql-schema node-shapes)
-       (spit "testSql.sql")))
+       (spit "testSql.sql"))
+
+  ;; CLI Playground.
+  ;; (def args ["--dx-profile" "/home/bartkl/Programming/alliander-opensource/metamorph/dev-resources/example_profile" "avro" "-o" "json"])
+
+  (def x (graph.db/resource conn (graph.avro/root-node-shape-uri conn)))
+
+  (get-in x [:id :id])
+  (avro-schema x)
+
+  (def args {:dx-profile "/home/bartkl/Programming/alliander-opensource/metamorph/dev-resources/example_profile", :format :json, :output :json, :_arguments []})
+  (generate-schema args)
+
+  (run-cmd* command-spec args))
---
+++ src/metamorph/graph/avro.clj
@@ -0,0 +1,20 @@
+; SPDX-FileCopyrightText: 2022 Alliander N.V.
+;
+; SPDX-License-Identifier: Apache-2.0
+
+(ns metamorph.graph.avro
+  (:require [deercreeklabs.lancaster :as l]
+    [asami.core :as d]
+    [metamorph.graph.db :as graph.db]
+    [metamorph.graph.shacl :as shacl]
+    [metamorph.rdf.datatype :refer [rdf-list->seq]]
+    [metamorph.schemas.avro.cardinality :refer [cardinality->schema-fn]]
+    [metamorph.schemas.avro.datatype :refer [xsd->avro]]
+    [metamorph.utils.uri :as utils.uri]))
+
+(defn root-node-shape-uri [conn]
+  (first (first
+          (d/q '[:find ?nodeShape
+                 :where [?nodeShape :rdf/type :sh/NodeShape]
+                 [?nodeShape :rdfs/comment "RootObject"]]
+            conn))))
