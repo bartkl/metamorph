6012fc044e928f6c68d6d6b61af11f887775fc4c
Bart Kleijngeld <bartkl@gmail.com>
2022-09-22 08:57:00 +0200

    Tidied up project.clj

--- project.clj
+++ project.clj
@@ -2,22 +2,24 @@
 ;
 ; SPDX-License-Identifier: Apache-2.0
 
-(defproject metamorph "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject metamorph "0.3.0"
+  :description "Metamorph is a Clojure libary that allows the generation of a variety of schemas from a DX-PROF profile."
+  :url "https://github.com/alliander-opensource/metamorph"
+  :license {:name "Apache License 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :plugins [[lein-cljfmt "0.8.2"]
             [io.taylorwood/lein-native-image "0.3.1"]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.eclipse.rdf4j/rdf4j-rio-turtle "4.0.0"]
-                 [deercreeklabs/lancaster "0.9.17"]
+                 [deercreeklabs/lancaster "0.9.20"]
                  [ont-app/vocabulary "0.1.7"]
                  [cli-matic "0.5.3"]
                  [org.clojars.quoll/asami "2.3.2"]
                  [com.github.seancorfield/honeysql "2.2.891"]]
-  :native-image {:name "metamorph"                 ;; name of output image, optional
-                 :graal-bin "/path/to/graalvm/home/bin/" ;; path to GraalVM home, optional
-                 :opts ["--verbose" "--no-fallback"]}           ;; pass-thru args to GraalVM native-image, optional
+  :native-image {:name "metamorph"
+                 :opts ["--verbose" "--no-fallback"
+                        "--initialize-at-build-time=com.fasterxml.jackson.core,com.fasterxml.jackson.dataformat"]}
   :main ^:skip-aot metamorph.core
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[lambdaisland/kaocha "1.69.1069"]
+                                  [com.github.clj-easy/graal-build-time "0.1.4"]]}})
